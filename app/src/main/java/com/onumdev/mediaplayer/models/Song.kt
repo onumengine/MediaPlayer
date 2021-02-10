d017d328a30e15865b268235cb88631c5621c86b
onumengine <onumdev@gmail.com>
2021-02-10 04:07:54 +0100

    Created the Song model

---
+++ app/src/main/java/com/onumdev/mediaplayer/models/Song.kt
@@ -0,0 +1,11 @@
+package com.onumdev.mediaplayer.models
+
+data class Song (
+        val id: Long,
+        val title: String,
+        var artist: String,
+        var album: String,
+        var albumTrackNumber: Int,
+        var genre: String,
+        val filePath: String
+)
